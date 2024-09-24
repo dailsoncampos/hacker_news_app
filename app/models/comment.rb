74445e4b85e19cad1b1f5e19b530e40262f828ff
Dailson Campos <dcampos.dev@gmail.com>
2024-09-24 16:09:19 -0300

    adicionada validação de story

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :story
 
-  validates :hacker_news_id, presence: true, uniqueness: true
-  validates :author, presence: true
-  validates :text, presence: true, length: { minimum: 20 }
+  validates :story, :hacker_news_id, :author, :text, presence: true
 end
